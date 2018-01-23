28bc9fd12b21a6912dbced17f2b62b87aac3f860
Dmitry Pasieka <dmitry.pasieka@gmail.com>
2018-01-22 21:05:42 -0800

    Add solution to a Programming Assignment from "Week 3"

---
+++ hw3_bench/fast.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
+	"io"
+	"os"
+	"strings"
+)
+
+type User struct {
+	Browsers []string `json:"browsers"`
+	Name     string   `json:"name"`
+	Email    string   `json:"email"`
+}
+
+func FastSearch(out io.Writer) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	seenBrowsers := make(map[string]bool)
+	fmt.Fprintln(out, "found users:")
+
+	userId := 0
+	fileScanner := bufio.NewScanner(file)
+	for fileScanner.Scan() {
+		user := User{}
+		err := user.UnmarshalJSON(fileScanner.Bytes())
+		if err != nil {
+			panic(err)
+		}
+
+		isAndroid := false
+		isMSIE := false
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				if _, ok := seenBrowsers[browser]; !ok {
+					seenBrowsers[browser] = true
+				}
+			}
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				if _, ok := seenBrowsers[browser]; !ok {
+					seenBrowsers[browser] = true
+				}
+			}
+		}
+
+		if isAndroid && isMSIE {
+			email := strings.Replace(user.Email, "@", " [at] ", -1)
+			fmt.Fprintln(out, fmt.Sprintf("[%d] %s <%s>", userId, user.Name, email))
+		}
+		userId++
+	}
+
+	fmt.Fprintln(out)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+}
+
+func Decode(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeString()
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "name":
+			out.Name = string(in.String())
+		case "email":
+			out.Email = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+
+func Encode(out *jwriter.Writer, in User) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"browsers\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+			out.RawString("null")
+		} else {
+			out.RawByte('[')
+			for v2, v3 := range in.Browsers {
+				if v2 > 0 {
+					out.RawByte(',')
+				}
+				out.String(string(v3))
+			}
+			out.RawByte(']')
+		}
+	}
+	{
+		const prefix string = ",\"name\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Name))
+	}
+	{
+		const prefix string = ",\"email\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Email))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v User) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	Encode(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v User) MarshalEasyJSON(w *jwriter.Writer) {
+	Encode(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	Decode(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	Decode(l, v)
+}
